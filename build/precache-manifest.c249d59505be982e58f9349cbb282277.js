0c8d2ee74e09679f14eaad37b9c604db91fa8f4c
ywaaang <yanbin.wyb@gmail.com>
2024-04-03 18:28:22 +0800

    initial commit

---
+++ build/precache-manifest.c249d59505be982e58f9349cbb282277.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7584aa1a23be7f11f4d9e6e3674e778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e48dbd0532db70afb0",
+    "url": "/static/js/main.df0621e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
